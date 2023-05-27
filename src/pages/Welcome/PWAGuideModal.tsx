ed75f2bc1c31ff76240b23838804ddfae04c171e
Inhwa Kim <radiantbeing99@gmail.com>
2023-05-27 21:01:26 +0900

    chore: 모달 텍스트 변경

--- src/pages/Welcome/PWAGuideModal.tsx
+++ src/pages/Welcome/PWAGuideModal.tsx
@@ -81,17 +81,17 @@ function PWAGuideModal() {
     <>
       <NavigateButton onClick={onOpen}>시작하기</NavigateButton>
 
-      <Modal isOpen={isOpen} onClose={onClose} size={"xs"} isCentered>
+      <Modal isOpen={isOpen} onClose={onClose} size={["xs", "md"]} isCentered>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>PWA 활성화 안내</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
             <Text>
-              Nutt는 PWA에 최적화되어 있습니다. PWA는 앱처럼 설치가 가능하며,
-              빠른 속도와 부드러운 사용자 경험을 제공합니다.
+              PWA를 지원하는 Nutt는 앱처럼 설치가 가능하며, 빠른 속도와 부드러운
+              사용자 경험을 제공합니다. 다음 단계를 따라 PWA를 활성화해보세요.
             </Text>
-            <Text>다음 단계를 따라 PWA를 활성화해보세요.</Text>
+
             <br />
             <PWAGuideAccordion />
           </ModalBody>
