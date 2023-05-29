06cf3f7f269d80bc2a7d2d5e21a7cb782ada89ad
Inhwa Kim <radiantbeing99@gmail.com>
2023-05-29 12:13:36 +0900

    chore: Drawer 사이즈 변경

--- src/pages/MealRecord/MealDrawer.tsx
+++ src/pages/MealRecord/MealDrawer.tsx
@@ -34,6 +34,37 @@ interface Meal {
   };
 }
 
+function translator(name: string) {
+  const names: any = {
+    Mandu: "만두",
+    KKennip: "깻잎",
+    Jabgokbab: "잡곡밥",
+    Jeyukbokum: "제육볶음",
+    Gimchizzigae: "김치찌개",
+    Samgyupsal: "삼겹살",
+    Duinjangzzigae: "된장찌개",
+    Gamjatang: "감자탕",
+    Ramyun: "라면",
+    Pizza: "피자",
+    Yangnyumchicken: "양념치킨",
+    Friedchicken: "후라이드치킨",
+    BaechuKimchi: "배추김치",
+    Kkakdugi: "깍두기",
+    Bulgogi: "불고기",
+    Godeungeogui: "고등어구이",
+    Zzajangmyun: "짜장면",
+    Zzambbong: "짬뽕",
+    Friedegg: "계란후라이",
+    Gyeranjjim: "계란찜",
+  };
+
+  if (names.hasOwnProperty(name)) {
+    return names[name];
+  } else {
+    return name;
+  }
+}
+
 type MealDrawerProps = {
   isOpen: boolean;
   onClose: () => void;
@@ -86,7 +117,7 @@ function MealDrawer({ isOpen, onClose, meal }: MealDrawerProps) {
   const { date, img, info } = meal;
   const [year, month, day] = date.split("-");
   const question = `오늘은 ${info.foods
-    .map((food) => food.name)
+    .map((food) => translator(food.name))
     .join(", ")}을 먹었어. 영양학적 측면에서 평가해줘.`;
 
   let mealTime = null;
@@ -106,7 +137,12 @@ function MealDrawer({ isOpen, onClose, meal }: MealDrawerProps) {
   }
 
   return (
-    <Drawer placement="right" onClose={onClose} isOpen={isOpen}>
+    <Drawer
+      placement="right"
+      onClose={onClose}
+      isOpen={isOpen}
+      size={["xs", "md"]}
+    >
       <DrawerOverlay />
       <DrawerContent>
         <DrawerCloseButton />
